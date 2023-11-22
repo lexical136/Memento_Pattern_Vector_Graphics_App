6405fcb46748e1f8ae42a78e87394bca7320185a
lexical136 <92440643+lexical136@users.noreply.github.com>
2023-11-22 18:43:03 +0000

    Updated app to use memento design pattern

---
+++ Vector_Graphics_App_v2/MementoClass.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Vector_Graphics_App_v2.ShapeClass;
+
+namespace Vector_Graphics_App_v2
+{
+    internal class MementoClass
+    {
+        // Originator class
+        public class Originator
+        {
+            public List<Shape> state;
+
+            public void setState(List<Shape> newState)
+            {
+                state = newState;
+            }
+
+            public Memento saveToMemento()
+            {
+                return new Memento(state);
+            }
+
+            public List<Shape> restoreFromMemento(Memento memento)
+            {
+                state = memento.getSavedList();
+
+                return state;
+            }
+        }
+
+        // Memento class
+        public class Memento
+        {
+            public List<Shape> state;
+
+            //Setter
+            public Memento(List<Shape> savedState)
+            {
+                state = savedState;
+            }
+
+            //Getter
+            public List<Shape> getSavedList()
+            {
+                return state;
+            }
+        }
+
+        // Caretaker class
+        public class Caretaker
+        {
+            private Stack<Memento> pastMementos = new Stack<Memento>();
+            private Stack<Memento> futureMementos = new Stack<Memento>();
+
+            public int getPastCount()
+            {
+                return pastMementos.Count;
+            }
+
+            public int getFutureCount()
+            {
+                return futureMementos.Count;
+            }
+
+            public void addPastMemento(Memento memento)
+            {
+                pastMementos.Push(memento);
+            }
+
+            public void addFutureMemento(Memento memento)
+            {
+                futureMementos.Push(memento);
+            }
+
+            public Memento getPastMemento()
+            {
+                Memento memento = pastMementos.Pop();
+                return memento;
+            }
+
+            public Memento getFutureMemento()
+            {
+                Memento memento = futureMementos.Pop();
+                return memento;
+            }
+
+            public void clearPastMementos()
+            {
+                pastMementos = new Stack<Memento>();
+            }
+
+            public void clearFutureMementos()
+            {
+                futureMementos = new Stack<Memento>();
+            }
+        }
+    }
+}
---
+++ Vector_Graphics_App_v2/ShapeClass.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vector_Graphics_App_v2
+{
+    internal class ShapeClass
+    {
+        public class Shape
+        {
+
+        }
+
+        public class Rectangle : Shape
+        {
+            public Rectangle(string n, int x, int y, int w, int h, string lin, string fil)
+            {
+                N = n;
+                X = x;
+                Y = y;
+                W = w;
+                H = h;
+                LIN = lin;
+                FIL = fil;
+
+            }
+            public string N { get; set; }
+            public int X { get; set; }
+            public int Y { get; set; }
+            public int W { get; set; }
+            public int H { get; set; }
+            public string LIN { get; set; }
+            public string FIL { get; set; }
+        }
+
+        //Circle(r, cx, cy, z)
+        public class Circle : Shape
+        {
+            public Circle(string n, int r, int cx, int cy, string lin, string fil)
+            {
+                N = n;
+                R = r;
+                CX = cx;
+                CY = cy;
+                LIN = lin;
+                FIL = fil;
+
+            }
+            public string N { get; set; }
+            public int R { get; set; }
+            public int CX { get; set; }
+            public int CY { get; set; }
+            public string LIN { get; set; }
+            public string FIL { get; set; }
+        }
+
+        //Ellipse(rx, ry, cx, cy, z)
+        public class Ellipse : Shape
+        {
+            public Ellipse(string n, int rx, int ry, int cx, int cy, string lin, string fil)
+            {
+                N = n;
+                RX = rx;
+                RY = ry;
+                CX = cx;
+                CY = cy;
+                LIN = lin;
+                FIL = fil;
+
+            }
+            public string N { get; set; }
+            public int RX { get; set; }
+            public int RY { get; set; }
+            public int CX { get; set; }
+            public int CY { get; set; }
+            public string LIN { get; set; }
+            public string FIL { get; set; }
+        }
+
+        //Line(x1, y1, x2, y2, z)
+        public class Line : Shape
+        {
+            public Line(string n, int x1, int y1, int x2, int y2, string lin, string fil)
+            {
+                N = n;
+                X1 = x1;
+                Y1 = y1;
+                X2 = x2;
+                Y2 = y2;
+                LIN = lin;
+                FIL = fil;
+
+            }
+            public string N { get; set; }
+            public int X1 { get; set; }
+            public int Y1 { get; set; }
+            public int X2 { get; set; }
+            public int Y2 { get; set; }
+            public string LIN { get; set; }
+            public string FIL { get; set; }
+        }
+
+        //polyline(x1, y1, x2, y2..., z)
+        public class Polyline : Shape
+        {
+            public Polyline(string n, List<int> xy, string lin, string fil)
+            {
+                N = n;
+                XY = xy;
+                LIN = lin;
+                FIL = fil;
+            }
+            public string N { get; set; }
+            public List<int> XY { get; set; }
+            public string LIN { get; set; }
+            public string FIL { get; set; }
+        }
+
+        //polygon(x1, y1, x2, y2... connects, z)
+        public class Polygon : Shape
+        {
+            public Polygon(string n, List<int> xy, string lin, string fil)
+            {
+                N = n;
+                XY = xy;
+                LIN = lin;
+                FIL = fil;
+            }
+            public string N { get; set; }
+            public List<int> XY { get; set; }
+            public string LIN { get; set; }
+            public string FIL { get; set; }
+        }
+    }
+}
